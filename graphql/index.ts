b1a5a36eca8db44ad21dfc49d842c9f8d596d248
James Quigley <jamesq@axosoft.com>
2019-01-14 10:15:00 -0700

    Change paths

--- graphql/index.ts
+++ graphql/index.ts
@@ -6,7 +6,7 @@ import schema from './schema';
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+app.use('/*', graphqlHTTP({
   schema,
   graphiql: process.env.NODE_ENV !== 'production'
 }));
